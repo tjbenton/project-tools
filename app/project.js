5abcdd5160a3a5bcdb3034f26e765f6128aa7604
Tyler Benton <tjbenton21@gmail.com>
2016-09-30 15:00:36 -0400

    `project.init`

--- app/project.js
+++ app/project.js
@@ -1,45 +1,71 @@
+import path from 'path'
+import fs from 'fs-extra-promisify'
+import { version } from '../package.json'
+import { exec } from './utils'
+import to from 'to-js'
+
 export default class Project {
   constructor(options = {}) {
-
+    this.options = options
+    this.root = this.options.root = options.root || process.cwd()
   }
 
-  async init() {
+  async init(project_name, location) {
+    const folder = path.resolve(`${__dirname}/../project-init`)
+    const author_info = await Promise.all([
+      exec('git config user.name'),
+      exec('git config user.email')
+    ])
 
+    let [ author_name, author_email ] = await author_info
+
+    await fs.copy(folder, location)
+
+    // update the json file with the current authors information
+    let file = await fs.readFile(`${location}/package.json`)
+    file = file.toString().replace(/\${([a-z_]*)}/g, (match, variable) => {
+      return { project_name, author_name, author_email, version }[variable]
+    })
+    await fs.outputFile(`${location}/package.json`, file, { spaces: 2 })
+  }
+
+  async projects() {
+    // asy
   }
 
   async create() {
-
+    console.log('create');
   }
 
   async build() {
-
+    console.log('build');
   }
 
   async start() {
-
+    console.log('start');
   }
 
   async stop() {
-
+    console.log('stop');
   }
 
   async watch() {
-
+    console.log('watch');
   }
 
   async list() {
-
+    console.log('list');
   }
 
   async use() {
-
+    console.log('use');
   }
 
   async publish() {
-
+    console.log('publish');
   }
 
   async translate() {
-
+    console.log('translate');
   }
 }
--- test/project.test.js
+++ test/project.test.js
@@ -1,23 +1,71 @@
+/* eslint-disable id-length, no-shadow */
+
 import ava from 'ava-spec'
-const test = ava.group('Project -')
+import fs from 'fs-extra-promisify'
+import path from 'path'
+import Project from '../dist/project.js'
+let test = ava.serial.group('Project -')
 
 
-test.todo('init')
 
-test.todo('create')
+test('functions exist', (t) => {
+  const methods = [ 'init', 'dirs', 'create', 'build', 'start', 'stop', 'watch', 'list', 'use', 'publish', 'translate' ]
 
-test.todo('build')
+  methods.forEach((method) => {
+    t.truthy(typeof Project.prototype[method] === 'function', `${method} is defined`)
+  })
+})
 
-test.todo('start')
+test('init', async (t) => {
+  const project = new Project()
+  const name = 'project-init-test'
+  const dir = path.join(__dirname, name)
 
-test.todo('stop')
+  await project.init(name, dir)
 
-test.todo('watch')
+  try {
+    await fs.stat(dir)
+    t.pass(`${dir} was copied`)
+  } catch (e) {
+    t.fail(`${dir} wasn't copied`)
+  }
 
-test.todo('list/ls')
+  await fs.remove(dir)
+})
 
-test.todo('use/save')
 
-test.todo('publish')
+test.serial.group('create', (test) => {
+  test.todo('create')
+})
 
-test.todo('translate')
+test.serial.group('build', (test) => {
+  test.todo('build')
+})
+
+test.serial.group('start', (test) => {
+  test.todo('start')
+})
+
+test.serial.group('stop', (test) => {
+  test.todo('stop')
+})
+
+test.serial.group('watch', (test) => {
+  test.todo('watch')
+})
+
+test.serial.group('list', (test) => { // ls
+  test.todo('list')
+})
+
+test.serial.group('use', (test) => { // save
+  test.todo('use')
+})
+
+test.serial.group('publish', (test) => {
+  test.todo('publish')
+})
+
+test.serial.group('translate', (test) => {
+  test.todo('translate')
+})

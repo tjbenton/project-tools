2122dee897582d6b0f2d03c87f9d6f3f5a507017
Tyler Benton <tjbenton21@gmail.com>
2017-08-28 12:15:25 -0400

    added the ability to use `i18next`
    This update also attempts to organize the code a little better into more managable functions that serve a specific purpose

--- app/compile/template/index.js
+++ app/compile/template/index.js
@@ -3,10 +3,13 @@ import { beautify } from '../../utils'
 import fs from 'fs-extra-promisify'
 import to from 'to-js'
 import templates from 'templates'
-import { forEach } from 'async-array-methods'
+import { forEach, map, reduce } from 'async-array-methods'
 import consolidate from 'consolidate'
+import globby from 'globby'
 import _ from 'lodash'
 import path from 'path'
+import i18n from 'i18next'
+
 const debug = require('debug')('compile:template')
 
 /// @name template
@@ -48,6 +51,7 @@ export default async function template(files, options = {}) { // eslint-disable-
     root: process.cwd(),
     globals: { _ },
     pretty: true,
+    fallback_locale: 'eng',
     languages: {
       txt: 'engine-base',
       html: 'engine-base',
@@ -62,12 +66,212 @@ export default async function template(files, options = {}) { // eslint-disable-
   app.create('layouts', { viewType: 'layout' })
   app.create('partials', { viewType: 'partial' })
 
-  app.option('cwd', options.root)
+  app.option('cwd', options.project_root)
 
-  ///# @todo {5} Add the ability to add data to the project
-  // const data = files.filter((file) => utils.ext(file) === 'json')
-  const template_files = files.filter((file) => utils.type(file) === 'template')
+  // holds the content files that're used for this project
+  const content_files = [ findContentFile(files) ]
 
+  if (options.layout) {
+    // convert the layout path to be absolute
+    options.layout = path.isAbsolute(options.layout) ? options.layout : path.resolve(options.root, options.layout)
+    // find the base folder for the layouts location so we can add all the layout files to the app
+    const [ folder ] = options.layout.replace(options.root + path.sep, '').split(path.sep)
+    const layout_files = await globby(folder ? `${options.root}/${folder}/**/*` : '_content.json', { nodir: true })
+    // find the layout content file and put it at the front so it will be overwritten by the project specific content
+    content_files.unshift(findContentFile(layout_files))
+    // add the layout files to the files array so they can be added to the app
+    files.push(...layout_files)
+  }
+
+  const template_files = files.filter((file) => utils.type(file) === 'template') // find only the template files
+  addTemplateLanguages(app, options, template_files) // adds all the template languages available
+
+  const resources = await resolveContent(content_files) // gets the content to use for the project
+
+  // reads all the template files and adds them to the app so they can be compiled
+  await addTemplateFiles(app, options, template_files, to.keys(resources))
+
+  // find the correct amount of spaces to indent the contents before a layout's applied
+  let spaces = app.find(options.layout, 'layouts')
+  spaces = spaces && spaces.content && spaces.content.match(/^(\s+){%\s*body\s*%}/gmi)
+  spaces = spaces ? spaces[0].replace(/{.*}/, '').length : 0
+
+  app.on('preLayout', (view) => {
+    if (spaces) {
+      // indent all the lines, then remove the padding from the starting line
+      view.contents = pad(view.contents, spaces).replace(/^\s*/, '')
+    }
+  })
+
+  // Note that this wouldn't be necessary if the `templates` lib supported
+  // `pug` and other indented languages better
+  app.on('preRender', fixIndentLang)
+  includeHelper(app)
+
+  debug('end')
+
+  ///# @name render
+  ///# @arg {string} file - the file path to compile
+  ///# @arg {object} locals - Local variables to be accessible in the templates
+  ///#
+  ///# @returns {object}
+  ///# ```js
+  ///# {
+  ///#   locale: '', // the locale that was used to compile the file
+  ///#   code: '', // the compiled code for the file
+  ///#   map: '', // the sourcemap for the file
+  ///#   language: '' // the language it was compile from
+  ///# }
+  ///# ```
+  ///# @async
+  return (file, locals = {}) => {
+    debug('render:start')
+    locals.file = file
+
+    let { locales: build_locales } = locals
+    delete locals.locales
+    build_locales = to.array(build_locales, /[\s,]+/)
+
+    if (build_locales.includes('all')) {
+      build_locales = to.keys(resources)
+    }
+
+    function render(locale) {
+      const project_locales = to.clone(locals)
+
+      // create a new instance of i18n so that multiple builds can run at the same time
+      const i18nInstance = i18n.createInstance()
+      i18nInstance.init({
+        resources,
+        lng: locale || options.fallback_locale,
+        fallbackLng: options.fallback_locale, // if a language doesn't have a key defined then it will fallback to the one set here
+        interpolation: { escapeValue: false }, // doesn't escape html characters so html can be appart of the content
+        returnObjects: true, // returns arrays and objects as arrays and objects
+        initImmediate: false, // since we aren't loading the data, we don't need it to be async
+      })
+
+      // add the i18n.t function as helpers in the app
+      const translate = i18nInstance.t.bind(i18nInstance)
+
+      // add the i18n.t function to the locals for other languages like pug
+      project_locales.t = project_locales.translate = translate // eslint-disable-line id-length
+      app.helper('t', translate)
+      app.helper('translate', translate)
+
+
+      return new Promise((resolve, reject) => {
+        app.render(getLocaleFile(file, locale), project_locales, (err, page) => {
+          if (err) {
+            err.filename = file
+            if (typeof err.toJSON === 'function') {
+              delete err.src
+              delete err.toJSON
+            }
+            reject(err)
+            return
+          }
+
+          renderLayout(page)
+            .then((stuff) => {
+              let code = stuff.content
+
+              if (options.pretty) {
+                code = beautify(code, 'html')
+              }
+
+              resolve({
+                locale: to.keys(resources).length ? locale : null,
+                code,
+                map: '',
+                language: utils.ext(file),
+              })
+            })
+            .catch(reject)
+        })
+      })
+    }
+
+    if (build_locales.length) {
+      return map(build_locales, render)
+    }
+
+    return render()
+  }
+}
+
+
+
+/// @name resolveContent
+/// @description This will take the content files that're passed to it ad read
+/// them and modify them to conform to i18n standard of namespacing
+/// @arg {array} files [[]] - The files to read
+/// @returns {object} - The object of content that has been merged together
+function resolveContent(files = []) {
+  return reduce(files.filter(Boolean), async (prev, file) => {
+    const contents = await fs.readJson(file)
+    for (const [ locale, content ] of to.entries(contents)) {
+      // if the keys don't include translation then add it in there
+      if (to.keys(content)[0] !== 'translation') {
+        contents[locale] = { translation: content }
+      }
+    }
+    return to.extend(prev, contents)
+  }, {})
+}
+
+
+/// @name findContentFile
+/// @description This is used to find content files
+/// @arg {array} files - The files to look through
+/// @returns {string, null} - if a content file exists then a string is returned
+function findContentFile(files = []) {
+  return files.filter((file) => file.includes('_content.json'))[0] || null
+}
+
+
+/// @name fixIndentLang
+/// @description This fixes weird issues with indented languages
+/// @note that this wouldn't be necessary if the `templates` lib supported `pug` and other indented languages better
+/// @returns {function}
+/// @access private
+function fixIndentLang(item) {
+  if ([ 'pug', 'jade' ].includes(utils.ext(item.key))) {
+    const str = item.contents.toString()
+    const script_regex = /<script(?:\s+[^>]*)?>((?:.|\n)*?)<\/script(\s+[^>]*)?>/g
+    str.replace(script_regex, (match, content) => {
+      if (content.split('\n').slice(1).filter((line) => line.trim()).length) {
+        throw new Error('Inline JS with multiple lines is not supported with `pug`, and `jade` files')
+      }
+
+      return content
+    })
+
+    item.contents = str
+      // minify each <style> tag into their own single line so that pug/jade doesn't break
+      .replace(/(<style(?:\s+[^>]*)?>)((?:.|\n)*?)(<\/style(\s+[^>]*)?>)/g, (match, open, content, close) => {
+        content = content
+          // remove the last `;` from each property list
+          .replace(/;(?=(\s|\n)*})/g, '')
+          // remove the space between these characters
+          .replace(/\s*([{}:;,]|!important)\s*/g, '$1')
+          .trim()
+        return `${open}${content}${close}\n`
+      })
+      // remove the space before each line that starts with `<` otherwise pug/jade breaks
+      .split(/\n/)
+      .map((line) => /^\s*</.test(line) ? line.trim() : line)
+      .join('\n')
+  }
+}
+
+
+/// @name addTemplateLanguages
+/// @description This adds all the languages avaliable to the app
+/// @arg {Templates} app - the app
+/// @arg {object} options - the options
+/// @arg {array} files - the template files
+/// @access private
+function addTemplateLanguages(app, options, template_files) {
   // and any languages that aren't specifcally defined
   to.unique(template_files.map(utils.ext))
     .filter((lang) => options.languages[lang] == null)
@@ -84,7 +288,32 @@ export default async function template(files, options = {}) { // eslint-disable-
 
     app.engine(language, consolidate[pkg] || require(pkg), language_options)
   }
+}
 
+
+
+/// @name getLocaleFile
+/// @description This is used to get the locale file path to use since the templates lib
+/// is weird about rendering the same page with different locales
+/// @arg {stirng} file - The file path to use
+/// @arg {string, undefined} locale - the locale to use if it's needed
+/// @returns {string} the same file that was passed if no locale was passed or the file path with the locale
+function getLocaleFile(file, locale) {
+  if (!locale) return file
+
+  const parsed_file = path.parse(file)
+  parsed_file.base = parsed_file.base.replace(parsed_file.ext, `____${locale}${parsed_file.ext}`)
+  return path.format(parsed_file)
+}
+
+/// @name addTemplateFiles
+/// @description This adds the views to the app
+/// @arg {Templates} app - the app
+/// @arg {object} options - the options
+/// @arg {array} files - the template files
+/// @access private
+/// @async
+async function addTemplateFiles(app, options, template_files, locales = []) {
   const ignore = template_files.filter(utils.shouldIgnore)
   const views = {}
   views.layouts = ignore.filter((file) => path.basename(file).includes('layout.'))
@@ -94,19 +323,41 @@ export default async function template(files, options = {}) { // eslint-disable-
   await forEach(_.keys(views), async (type) => {
     await forEach(views[type], async (page) => {
       const content = to.string(await fs.readFile(page))
-      const info = {
-        path: page,
-        content,
+
+      function addFile(file) {
+        const info = {
+          path: file,
+          content,
+        }
+
+        // if the type is a page, then set the layout to be the layout that was passed in
+        if (type === 'pages' && !!options.layout) {
+          info.layout = options.layout
+        }
+
+        app[type.slice(0, -1)](file, info)
       }
 
-      if (type === 'pages' && !!options.layout) {
-        info.layout = options.layout
-      }
 
-      app[type.slice(0, -1)](page, info)
+
+      // always add the original file
+      addFile(page)
+
+      // if the type is pages then add all the locale files
+      if (type === 'pages') {
+        for (const locale of locales) {
+          addFile(getLocaleFile(page, locale))
+        }
+      }
     })
   })
+}
 
+
+/// @name includeHelper
+/// @description This is used to add the include helper to the app
+/// @arg {Template} app - the app
+function includeHelper(app) {
   ///# @name include
   ///# @description This is used inside of any template file to import any other template file
   ///# @arg {string} file - The file path to the template to include
@@ -126,9 +377,7 @@ export default async function template(files, options = {}) { // eslint-disable-
       return cb(null, `<!-- can't find import ${file} -->`)
     }
 
-    const ctx = _.extend({}, this.context || {}, locals || {}, view.ctx || {})
-
-    view.compile()
+    const ctx = to.extend(to.extend(to.extend({}, this.context || {}), locals || {}), view.ctx || {})
 
     view.render(ctx, (err, { content }) => {
       if (err) {
@@ -138,88 +387,46 @@ export default async function template(files, options = {}) { // eslint-disable-
       cb(null, content)
     })
   })
+}
 
-  debug('end')
+/// @name pad
+/// @description pad each line with spaces
+/// @arg {string} str - the string to pad with characters
+/// @arg {number} amount - the amount of characters you put infront of each line
+/// @arg {string} character [' '] - the character
+/// @returns {string} the padded string with characters
+function pad(str, amount, character = ' ') {
+  const characters = _.repeat(character, amount)
+  return str.toString()
+    .split('\n')
+    .map((line) => `${characters}${line}`)
+    .join('\n')
+}
 
-  ///# @name render
-  ///# @arg {string} file - the file path to compile
-  ///# @arg {object} locals - Local variables to be accessible in the templates
-  ///#
-  ///# @returns {object}
-  ///# ```js
-  ///# {
-  ///#   code: '', // the compiled code for the file
-  ///#   map: '', // the sourcemap for the file
-  ///#   language: '' // the language it was compile from
-  ///# }
-  ///# ```
-  ///# @async
-  return (file, locals = {}) => {
-    debug('render:start')
-    return new Promise((resolve, reject) => {
+
+/// @name renderLayout
+/// @description this will render the layout file with correct data
+/// @arg {View} page - This is the view class
+/// @returns {View} - With the updated contents
+/// @async
+function renderLayout(page) {
+  return new Promise((resolve, reject) => {
+    // If the page has a layout applied to it then render the layout with the correct engine.
+    // This is nessisary because the templates lib doesn't compile the layout file
+    if (page.layoutStack && page.layoutStack.length) {
+      const file = page.layoutStack[0].layout.key
+      page.engine = `.${utils.ext(file)}`
+
+      const locals = page.localsStack.reduce((prev, next) => to.extend(prev, next), {})
       locals.file = file
 
-      const view = app.find(file)
+      page.render(locals, (error, view) => {
+        if (error) return reject(error)
 
-      // Note that this wouldn't be necessary if the `templates` lib supported
-      // `pug` and other indented languages better
-      app.on('preRender', (item) => {
-        if ([ 'pug', 'jade' ].includes(utils.ext(item.key))) {
-          const str = item.contents.toString()
-          const script_regex = /<script(?:\s+[^>]*)?>((?:.|\n)*?)<\/script(\s+[^>]*)?>/g
-          str.replace(script_regex, (match, content) => {
-            if (content.split('\n').slice(1).filter((line) => line.trim()).length) {
-              reject(new Error('Inline JS with multiple lines is not supported with `pug`, and `jade` files'))
-              return ''
-            }
-
-            return content
-          })
-
-          item.contents = str
-            // minify each <style> tag into their own single line so that pug/jade doesn't break
-            .replace(/(<style(?:\s+[^>]*)?>)((?:.|\n)*?)(<\/style(\s+[^>]*)?>)/g, (match, open, content, close) => {
-              content = content
-                // remove the last `;` from each property list
-                .replace(/;(?=(\s|\n)*})/g, '')
-                // remove the space between these characters
-                .replace(/\s*([{}:;,]|!important)\s*/g, '$1')
-                .trim()
-              return `${open}${content}${close}\n`
-            })
-            // remove the space before each line that starts with `<` otherwise pug/jade breaks
-            .split(/\n/)
-            .map((line) => /^\s*</.test(line) ? line.trim() : line)
-            .join('\n')
-        }
+        resolve(view)
       })
-
-      view.compile()
-
-      view.render(locals, (err, res) => {
-        if (err) {
-          err.filename = file
-          if (typeof err.toJSON === 'function') {
-            delete err.src
-            delete err.toJSON
-          }
-          reject(err)
-          return
-        }
-
-        let code = res.content
-
-        if (options.pretty) {
-          code = beautify(code, 'html')
-        }
-
-        resolve({
-          code,
-          map: '',
-          language: utils.ext(file),
-        })
-        debug('render:end')
-      })
-    })
-  }
+    } else {
+      resolve(page)
+    }
+  })
 }

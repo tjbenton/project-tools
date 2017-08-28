afa144349883dec857ab39c0ce6973b09b9befab
Tyler Benton <tjbenton21@gmail.com>
2017-08-28 12:16:01 -0400

    updated the base compile function to handle locale files better

--- app/compile/index.js
+++ app/compile/index.js
@@ -1,6 +1,6 @@
 import * as utils from './utils'
 import globby from 'globby'
-import { map } from 'async-array-methods'
+import { reduce } from 'async-array-methods'
 import to from 'to-js'
 import _ from 'lodash'
 import path from 'path'
@@ -8,7 +8,7 @@ const debug = require('debug')('compile')
 
 /// @name compile
 /// @page compile
-/// @arg {string} root - The root of folder to compile
+/// @arg {string} project_root - The root of folder to compile
 /// @arg {object} options
 ///
 /// ```js
@@ -25,7 +25,7 @@ const debug = require('debug')('compile')
 /// This will compile any file if it can be compiled
 ///
 /// @returns {function} render
-export default async function compile(root, options = {}) {
+export default async function compile(project_root, options = {}) {
   debug('start setup')
 
   options = to.extend({
@@ -36,7 +36,19 @@ export default async function compile(root, options = {}) {
     style: {},
     template: {},
     javascript: {},
+    layout: '',
+    rename(item, locale) {
+      const dist = item.path.replace(/\bapp\b/, 'dist')
+      if (item.processor === 'template' && locale) {
+        return dist.replace(`${item.file}`, path.join(locale, item.file))
+      }
+      return dist
+    },
   }, options)
+
+  // allow for layout to be set on the templates options and the base options
+  options.template.layout = options.template.layout || options.layout
+  options.template.root = options.root
 
   const processors = to.keys(utils.processors)
     .reduce((prev, next) => {
@@ -45,10 +57,11 @@ export default async function compile(root, options = {}) {
     }, {})
   const glob_options = { ignore: [ 'node_modules' ].concat(options.ignore), nodir: true }
 
-  const root_files = await globby(path.join(root, '**', '*'), glob_options)
+  const root_files = await globby(path.join(project_root, '**', '*'), glob_options)
 
-  function run(files) {
-    return map(files, async (file) => {
+  function run(files, locales) {
+    /* eslint-disable max-statements */
+    return reduce(files, async (prev, file) => {
       const debug_file = utils.color(file)
       debug(`start file ${debug_file}`)
       const type = utils.type(file)
@@ -66,27 +79,39 @@ export default async function compile(root, options = {}) {
       }
 
       const result = await processors[type](file, {
-        root,
+        project_root,
+        root: options.root,
         minify: options.minify,
         sourcemaps: options.sourcemaps,
         pretty: options.pretty,
+        fallback_locale: options.fallback_locale,
+        locales,
         ...opts,
       })
 
-      result.type = type
-      result.src = file
-      result.path = utils.renameExt(file)
-      result.root = root
-      result.file = result.path.slice(result.root.length + 1)
-      result.processor = type
+      for (const item of to.array(result)) {
+        item.type = type
+        item.path = utils.renameExt(file)
+        item.src = file
+        item.root = project_root
+        item.file = item.path.slice(item.root.length + 1)
+        item.processor = type
+        item.dist = options.rename(item, item.locale)
 
-      if (!options.sourcemaps) {
-        result.map = ''
+        if (typeof item.dist !== 'string') {
+          throw new Error(`the file must be a string, you return '${item.dist}' from 'options.rename'`)
+        }
+
+        if (!options.sourcemaps) {
+          item.map = ''
+        }
+
+        prev.push(item)
       }
 
       debug(`end file ${debug_file}`)
-      return result
-    })
+      return prev
+    }, [])
   }
 
   // run the initial setup for templates and reset it to call in the `run` function above
@@ -96,8 +121,9 @@ export default async function compile(root, options = {}) {
   ///# @name render
   ///# @description This function is used to render any glob that's passed to it
   ///# @arg {string} glob ['**/*'] - The glob to render
+  ///# @arg {array, string} locales [all] - the locales to build
   ///# @returns {array} - files that have been rendered
-  return async (glob = '**/*') => {
+  return async (glob = '**/*', locales = 'all') => {
     debug('start render')
     let files = await globby(glob, glob_options)
 
@@ -109,7 +135,8 @@ export default async function compile(root, options = {}) {
         return prev
       }, [ [], [] ])
 
-    files[0] = await run(files[0])
+    // run all the files through that aren't template files
+    files[0] = await run(files[0], locales)
 
     // add the compiled files on to the template globals so
     // that they will be available to the project
@@ -126,8 +153,8 @@ export default async function compile(root, options = {}) {
       }, {}),
     )
 
-    // render the rest of the files
-    files[1] = await run(files[1])
+    // render all the template files
+    files[1] = await run(files[1], locales)
 
     debug('end render')
     return to.flatten(files)

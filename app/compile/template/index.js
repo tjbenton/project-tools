dd5c533a4ce2eda3475b963b5176c161c7c1c945
Austin Meadows <austin@cheesy.pizza>
2018-02-13 15:03:14 -0500

    allow specifying locales when building to not build certain items in locales folder

--- app/compile/template/index.js
+++ app/compile/template/index.js
@@ -123,7 +123,15 @@ export default async function template(files, options = {}) { // eslint-disable-
 
     let { locales: locales_to_build } = locals
     delete locals.locales
-    locales_to_build = to.array(locales_regex.exec(file)[1] || locales_to_build, /[\s,]+/)
+    // if we want to build all locales, if the file satisfies the locales_regex, then build each
+    // file according to its respective locale. Otherwise, use the specified locale in locals.locales.
+    if (locales_to_build === 'all') {
+      locales_to_build = to.array(locales_regex.exec(file)[1] || locales_to_build, /[\s,]+/)
+    // if we don't want to build all locales, then we still want to build items in a locales folder, just not the ones
+    // that aren't the specified locales.
+    } else {
+      locales_to_build = _.intersection(to.array(locales_regex.exec(file)[1]), to.array(locales_to_build, /[\s,]+/))
+    }
 
     let resources
     // if the `initial_resources` is defined then use it then set it to null
